be3078ba112656f3e349461b18ee137d6147308e
Abhishek <abhishekdaszy@gmail.com>
2024-01-22 17:33:43 +0530

    added

--- Smallest.java
+++ Smallest.java
@@ -1,3 +1,4 @@
+import java.util.*;
 class Smallest 
 {
     public int findSmallest(int arr[])
@@ -12,10 +13,17 @@ class Smallest
         }
         return smallest;
     }
+    public int SmallestBysortArray(int arr[])
+    {
+        Arrays.sort(arr);
+        return arr[0]; 
+    }
     public static void main(String[] args) 
     {
         int arr [] = {3,4,1,2,5};
         Smallest s = new Smallest();
         System.out.println(s.findSmallest(arr));
+
+        System.out.println(s.SmallestBysortArray(arr));
     }
 }
